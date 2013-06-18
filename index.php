783ffb57cb3ac44d9036de5b072b45fd8f2b5036
Sam Frank <rust@0xio.org>
2013-06-17 23:37:04 -0700

    updated main index

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>microboard title</title>
+		<title>Site Title</title>
 		<link rel="stylesheet" type="text/css" href="static/style.css" />
 	</head>
 	
 	<body>
+		<div id="content">
+			<h1>Microboard - Site Title </h1>
+			<div class="nav">[ / <a href="board">board</a> / ]</div>
+			<hr>
+			<b>microboard</b> is a simple message board solution developed to be stream-lined and simple to use.
+			Simply select the board you would like to post on, submit a thread, or click on a thread and reply to it.
+			All posts are anonymous, and only date and time are recorded. Be sure to follow the rules of the board! 
+			If you would like to download <b>microboard</b>, you can clone it from our <a href="https://github.com/baka-rust/microboard">git repo</a>.
+		</div>
 	</body>
 </html>
