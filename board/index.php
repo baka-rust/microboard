bbbf3cd8206daf919fad04bfcf928c232c29bacb
Sam Frank <rust@0xio.org>
2013-06-17 23:24:22 -0700

    first push

---
+++ board/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>/board/ - Board Title</title>
+		<link rel="stylesheet" type="text/css" href="../static/style.css" />
+	</head>
+	
+	<body>
+		<div id="content">
+		
+			<h1>/board/ - Board Title</h1>
+		
+			<hr>
+		
+			<div class="thread" id="1">
+				<span class="post-info">Month Day, Year @ Time <a href="#number">#number</a></span>
+				<hr class="post">
+				thread content
+			</div>
+			
+			<div class="post" id="2">
+				<span class="post-info">Month Day, Year @ Time <a href="#number">#number</a></span>
+				<hr class="post">
+				reply content
+			</div>
+		
+		</div>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>microboard title</title>
+		<link rel="stylesheet" type="text/css" href="static/style.css" />
+	</head>
+	
+	<body>
+	</body>
+</html>
