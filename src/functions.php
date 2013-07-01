db0bd781709a99096e7e877ce2e1f5d9ff944066
Sam Frank <rust@bytelife.org>
2013-07-01 14:33:51 -0700

    finished GetThreads function

--- src/functions.php
+++ src/functions.php
@@ -12,11 +12,23 @@
 		}
 	}
 	
-	function GetThreads($board, $page) {
-		$StartRange = ($page * 10) - 10
-		$EndRange = $page * 10
+	function GetThreads($Board, $Page) {
+		$StartRange = ($Page * 10) - 10;
+		$EndRange = $Page * 10;
 		
-		$STH = $DBH->query('SELECT * FROM threads'); //the "threads" table will change to the name of the board
+		$STH = $DBH->prepare('SELECT * FROM ? WHERE parent="0" LIMIT ?, ?'); //parent being the parent thread to a reply, 0 meaning its a thread
+		$STH->execute(array($board, $StartRange, $EndRange));
+		$Rows = $STH->fetchAll(PDO::FETCH_ASSOC);
+		
+		foreach($Rows as $Row) {
+			print('
+					<div class="thread" id="'.$Row["id"].'">
+						<span class="post-info"> '.$Row["date"].' @ '.$Row["time"].' <a href="#'.$Row["id"].'">#'.$Row["id"].'</a> <span class="more">[<a href="thread.php?id='.$Row["id"].'">more</a>]</span></span>
+						<hr class="post">
+						'.$Row["content"].'
+					</div>
+			');	//change second $Row["id"] to javascript to insert quote into postbox for that id
+		}
 	}
 
 ?>
